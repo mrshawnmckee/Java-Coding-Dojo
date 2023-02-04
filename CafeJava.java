880ca439bf9c682560e1550ebeecd0bf0955035d
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2023-02-04 14:39:57 -0500

    Add files via upload

---
+++ CafeJava.java
@@ -0,0 +1,38 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready. ";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffee = 2.75;
+        double latte = 3.75;
+        double cappucino = 4.25;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = false;
+        boolean isReadyOrder3 = true;
+        boolean isReadyOrder4 = true;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
+        System.out.println(customer1 + pendingMessage);
+        System.out.println(isReadyOrder4 ? (customer4 + readyMessage + displayTotalMessage + cappucino) : (customer4 + pendingMessage));
+        System.out.println(displayTotalMessage + (latte * 2) + (isReadyOrder2 ? readyMessage : pendingMessage));
+        System.out.println(displayTotalMessage + (latte - dripCoffee));
+    }
+}
+
