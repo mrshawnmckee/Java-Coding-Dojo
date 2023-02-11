ea9bf9689f02411b495ae283635fd39bcd3bba73
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2023-02-11 12:32:02 -0500

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,23 @@
+//Practice with using Exceptions
+import java.util.ArrayList;
+
+public class Exceptions {
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+            for(int i = 0; i < myList.size(); i++) {
+                try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(i);
+            } catch (Exception e) {
+                System.out.println("That cannot be converted to an Integer");
+            } 
+        }
+    }
+}
+        
+    
